063632e46429ffe6a214f59779d3c77c65f4a615
Ross Gardiner <ross-public@gmx.com>
2017-06-09 03:33:41 +0100

    Working game

--- Main.elm
+++ Main.elm
@@ -5,9 +5,11 @@ import Collage exposing (..)
 import Element exposing (..)
 import Color exposing (..)
 import Keyboard exposing (..)
+import Random exposing (..)
 import Time exposing (..)
 import Css exposing (padding, px)
 import Char exposing (fromCode)
+import Text
 import Debug
 
 main =
@@ -32,16 +34,26 @@ type Direction
   | Left
   | Right
 
+type alias Game =
+  { snake: Snake
+  , orphan: Maybe Position
+  , time: Int
+  , score: Int }
+
 type alias Snake =
   { head: Position
   , tail: List Position
   , direction: Direction }
 
-type alias Model = Snake
+type alias Model = Game
 
 init : ( Model, Cmd Msg )
 init =
-  ( Snake (0, 0) [] Right, Cmd.none )
+  ( { snake = Snake (0, 0) [] Right
+    , orphan = Nothing
+    , time = 0
+    , score = 0 }
+  , Cmd.none )
 
 
 -- UPDATE
@@ -52,11 +64,18 @@ type Msg = KeyMsg Keyboard.KeyCode
 
 tickModel : Model -> Model
 tickModel model =
-  case model.direction of
-    Up -> {model | head = (Tuple.first model.head, (Tuple.second model.head + 1) % 20)}
-    Down -> {model | head = (Tuple.first model.head, (Tuple.second model.head - 1) % 20)}
-    Left -> {model | head = ((Tuple.first model.head - 1) % 20, Tuple.second model.head)}
-    Right -> {model | head = ((Tuple.first model.head + 1) % 20, Tuple.second model.head )}
+  let oldSnake = model.snake
+      newSnake = {oldSnake | head =
+        case oldSnake.direction of
+          Up -> (Tuple.first oldSnake.head, (Tuple.second oldSnake.head + 1) % 20)
+          Down -> (Tuple.first oldSnake.head, (Tuple.second oldSnake.head - 1) % 20)
+          Left -> ((Tuple.first oldSnake.head - 1) % 20, Tuple.second oldSnake.head)
+          Right -> ((Tuple.first oldSnake.head + 1) % 20, Tuple.second oldSnake.head )}
+      newOrphan = case model.orphan of
+        Nothing ->
+          Just (Tuple.first (Random.step (Random.int 0 19) (initialSeed model.time) ), Tuple.first (Random.step  (Random.int 0 19) (initialSeed (model.time*2))))
+        Just orphan -> if model.snake.head == orphan then Nothing else Just orphan
+  in { model | snake = newSnake, orphan = newOrphan, time = model.time + 1 }
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
@@ -64,14 +83,17 @@ update msg model =
     KeyMsg code ->
       -- let l = Debug.log "Key " (fromCode code)
       -- in (model, Cmd.none)
-      case fromCode code of
-        'W' -> ({model | direction = Up}, Cmd.none)
-        'A' -> ({model | direction = Left}, Cmd.none)
-        'S' -> ({model | direction = Down}, Cmd.none)
-        'D' -> ({model | direction = Right}, Cmd.none)
-        _  -> (model, Cmd.none)
+      let oldSnake = model.snake
+          newSnake = { oldSnake | direction = case fromCode code of
+            'W' -> Up
+            'A' -> Left
+            'S' -> Down
+            'D' -> Right
+            _ -> oldSnake.direction }
+      in ({ model | snake = newSnake }, Cmd.none)
     Tick newTime ->
       (tickModel model, Cmd.none)
+
 
 -- SUBSCRIPTIONS
 
@@ -86,16 +108,19 @@ subscriptions model =
 styles =
     Css.asPairs >> Html.Attributes.style
 
-gridToPx : Snake -> (Float, Float)
-gridToPx s =
-  let px1  = toFloat (Tuple.first s.head * segmentDim) - (width/2)
-      px2  = toFloat (Tuple.second s.head * segmentDim) - (height/2)
+gridToPx : Position -> (Float, Float)
+gridToPx pos =
+  let px1  = toFloat (Tuple.first pos * segmentDim) - (width/2)
+      px2  = toFloat (Tuple.second pos * segmentDim) - (height/2)
   in ( px1 + (segmentDim / 2), px2 + (segmentDim/2))
 
-snake : Snake -> Element
-snake s = (collage width height
+renderGame : Game -> Element
+renderGame game = (collage width height
             [ fittedImage width height "http://localhost:8000/wheat.jpg" |> toForm
-            , image (round (segmentDim * 1.5)) (round (segmentDim * 1.5)) "http://localhost:8000/scary.png" |> toForm |> move (gridToPx s)
+            , case game.orphan of
+                Just orphan -> image (round (segmentDim * 1.2)) (round (segmentDim * 1.2)) "http://localhost:8000/orphan.png" |> toForm |> move (gridToPx orphan)
+                Nothing     -> square 0 |> filled black
+            , image (round (segmentDim * 1.5)) (round (segmentDim * 1.5)) "http://localhost:8000/scary.png" |> toForm |> move (gridToPx game.snake.head)
             , segment (-width/2, -height/2) (-width/2, height/2) |> traced (solid black)
             , segment (-width/2, -height/2) (width/2, -height/2) |> traced (solid black)
             , segment (width/2, -height/2) (width/2, height/2) |> traced (solid black)
@@ -103,9 +128,10 @@ snake s = (collage width height
             ]
           )
 
-
 view : Model -> Html Msg
 view model =
   div
     [ styles [ padding (px 10) ] ]
-    [ (snake model) |> Element.toHtml ]
+    [ h1 [ styles [ Css.fontFamilies ["Impact"] ] ] [Html.text "Naughty Theresa"]
+    , p [] [Html.text (toString model.score)]
+    , (renderGame model) |> Element.toHtml ]
